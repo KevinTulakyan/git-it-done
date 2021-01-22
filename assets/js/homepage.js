2e193828b1cb28fc1bd5c36a5092d7b53cf9034d
KevinTulakyan <KevinTulakyan@gmail.com>
2021-01-21 17:37:33 -0800

    return page if nothing selected

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -38,6 +38,9 @@ var getUserRepos = function(user) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+  var repoEl = document.createElement("a");
+  repoEl.classList = "list-item flex-row justify-space-between align-center";
+  repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
   // check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
@@ -54,9 +57,9 @@ var displayRepos = function(repos, searchTerm) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     // create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-
+    repoEl.setAttribute("href", "./single-repo.html");
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
