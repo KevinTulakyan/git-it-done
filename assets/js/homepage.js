f3df8abd3d6a24a46a53bdedac2ed8bd687ae1f7
KevinTulakyan <KevinTulakyan@gmail.com>
2021-01-22 20:05:55 -0800

    added search function for css,  html, javascript

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
@@ -9,11 +10,11 @@ var formSubmitHandler = function(event) {
   var username = nameInputEl.value.trim();
   if (username) {
     getUserRepos(username);
+    repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
   }
-  
 };
 
 var getUserRepos = function(user) {
@@ -23,8 +24,10 @@ var getUserRepos = function(user) {
   // make a request to the url
   fetch(apiUrl).then(function(response) {
     if(response.ok){
+      console.log(response);
       response.json().then(function(data) {
-        displayRepos(data,user);
+        console.log(data);
+        displayRepos(data, user);
       });
     }
     else{
@@ -38,17 +41,12 @@ var getUserRepos = function(user) {
 };
 
 var displayRepos = function(repos, searchTerm) {
-  var repoEl = document.createElement("a");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
-  repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
   // check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
     return;
   }
-  console.log(repos);
-  console.log(searchTerm);
-  repoContainerEl.textContent = "";
+
   repoSearchTerm.textContent = searchTerm;
 
   // loop over repos
@@ -59,7 +57,7 @@ var displayRepos = function(repos, searchTerm) {
     // create a container for each repo
     var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-    repoEl.setAttribute("href", "./single-repo.html");
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
@@ -73,8 +71,7 @@ var displayRepos = function(repos, searchTerm) {
 
     // check if current repo has issues or not
     if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-      "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+      statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
     } 
     else {
       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
@@ -88,4 +85,29 @@ var displayRepos = function(repos, searchTerm) {
 }
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } 
+    else {
+      alert("Error: " + response.statusText);
+    }
+  })
+}
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+  if(language){
+    getFeaturedRepos(language);
+    repoContainerEl.textContent = "";
+  }
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
